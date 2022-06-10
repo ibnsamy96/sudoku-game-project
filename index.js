b54e320f9ead8d01a7bf497cd12ffa093c138e91
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-06-10 23:58:41 +0200

    added first two pages/sections

---
+++ index.js
@@ -0,0 +1,70 @@
+function invokeFirstPage() {
+	const loginForm = document.querySelector("form");
+
+	// loginForm.parentElement
+
+	loginForm.addEventListener("submit", function (event) {
+		event.preventDefault();
+		console.log(event.target);
+	});
+}
+
+// theme images carousal
+
+function invokeSecondPage() {
+	const themeSelectorElement = document.querySelectorAll(".theme-selector");
+
+	themeSelectorElement.forEach((el) => {
+		el.addEventListener("click", function () {
+			const currentTheme = document.body.dataset.theme;
+			document.querySelector("." + currentTheme).classList.remove("selected");
+			document.body.dataset.theme = this.classList[1];
+			this.classList.add("selected");
+		});
+	});
+
+	const carousalPreviousBTNs = document.querySelectorAll(".previous");
+	const carousalNextBTNs = document.querySelectorAll(".next");
+
+	function carousalMovement(event) {
+		event.stopPropagation();
+
+		const imagesElementContainer =
+			this.classList[0] === "previous"
+				? this.nextElementSibling
+				: this.previousElementSibling;
+		const imagesElement = imagesElementContainer.querySelector(".images");
+
+		let imagesElementCurrentLeft = imagesElement.style.left || "0px";
+		imagesElementCurrentLeft = parseInt(imagesElementCurrentLeft);
+
+		const imagesElementWidth = imagesElement.scrollWidth;
+		const containerElementWidth = imagesElement.parentElement.clientWidth;
+
+		if (this.classList[0] === "previous") {
+			if (imagesElementCurrentLeft + 100 >= 0) {
+				imagesElement.style.left = 0 + "px";
+			} else {
+				imagesElement.style.left = imagesElementCurrentLeft + 100 + "px";
+			}
+		} else {
+			if (
+				imagesElementCurrentLeft - 100 <=
+				-(imagesElementWidth - containerElementWidth)
+			) {
+				imagesElement.style.left =
+					-(imagesElementWidth - containerElementWidth) + "px";
+			} else {
+				imagesElement.style.left = imagesElementCurrentLeft - 100 + "px";
+			}
+		}
+	}
+
+	carousalPreviousBTNs.forEach((btn) => {
+		btn.addEventListener("click", carousalMovement);
+	});
+
+	carousalNextBTNs.forEach((btn) => {
+		btn.addEventListener("click", carousalMovement);
+	});
+}
